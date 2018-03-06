2fcfd8406c061927b73139bcfc0e92630430e4b9
KertM <kert.moor@gmail.com>
2018-03-06 09:27:22 +0200

    2

--- WpfList/MainWindow.xaml.cs
+++ WpfList/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace WpfList
             InitializeComponent();
 
             List<TodoItem> items = new List<TodoItem>();
-            //items.Add(new TodoItem() { Title = "Complete this WPF tutotrial", Completion = 45});
+            //items.Add(new TodoItem() { Title = "Complete this WPF tutotrial", Completion = 45 });
             //items.Add(new TodoItem() { Title = "Learn C#", Completion = 80 });
             //items.Add(new TodoItem() { Title = "Got to shop", Completion = 100 });
             //items.Add(new TodoItem() { Title = "Buy stuff", Completion = 10 });
@@ -44,12 +44,56 @@ namespace WpfList
             TodoListBox.ItemsSource = items;
 
         }
+        private void todoListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Title = (TodoListBox.SelectedItem as TodoItem).Title;
+        }
+        private void btnShowSelectedItem_Click(object sender, RoutedEventArgs e)
+        {
+            //if(TodoListBox.SelectedItem != null)
+            foreach (var item in TodoListBox.SelectedItems)
+            {
+                MessageBox.Show((item as TodoItem).Title);
+            }
+            MessageBox.Show((TodoListBox.SelectedItem as TodoItem).Title);
+        }
+        private void btnSelectLast_Click(object sender, RoutedEventArgs e)
+        {
+            TodoListBox.SelectedIndex = TodoListBox.Items.Count - 1;
+        }
+        private void btnSelectNext_Click(object sender, RoutedEventArgs e)
+        {
+            int nextIndex = 0;
+            if((TodoListBox.SelectedIndex >= 0) &&(TodoListBox.SelectedIndex < (TodoListBox.Items.Count -1)))
+            {
+                nextIndex = TodoListBox.SelectedIndex + 1;
+            }
+            TodoListBox.SelectedIndex = nextIndex;
+        }
+        private void btnSelectCSharp_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in TodoListBox.Items)
+            {
+                if((item is TodoItem) && ((item as TodoItem).Title.Contains("C#")))
+                {
+                    TodoListBox.SelectedItem = item;
+                }
+            }
+        }
+        private void btnSelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in TodoListBox.Items)
+            {
+                TodoListBox.SelectedItems.Add(item);
+            }
+        }
     }
     public class TodoItem
     {
+        public string Title { get; set; }
+        public int Completion { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Completion { get; set; }
     }
 
 
